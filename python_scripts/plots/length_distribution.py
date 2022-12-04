f3fb897df05545d0b1b1b77007e772d1e076dc1f
nilsho01 <s220672@dtu.dk>
2022-12-04 16:06:51 +0100

    length distribution plot

--- python_scripts/plots/length_distribution.py
+++ python_scripts/plots/length_distribution.py
@@ -1,14 +1,46 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+def length_distribution(sequencing_report_all, all_experiments = True):
+    if all_experiments == True:
+        unique_experiments = sequencing_report_all["Experiment"].unique()
+        unique_experiments = np.sort(unique_experiments)
+    # Subplots are organized in a Rows x Cols Grid
+    # Tot and Cols are known
+    Tot = unique_experiments.shape[0]
+    Cols = int(input("How many Rows for the Plots do you want?"))
+    # Compute Rows required
+    Rows = Tot // Cols
+    #     EDIT for correct number of rows:
+    #     If one additional row is necessary -> add one:
+    if Tot % Cols != 0:
+        Rows += 1
+    # Create a Position index
+    Position = range(1,Tot + 1)
+    n = 0
+    fig = plt.figure(1)
+    for experiment in unique_experiments:
+        batch = sequencing_report_all[sequencing_report_all["Experiment"] == experiment]
+        length = batch["aaSeqCDR3"].str.len()
+        unique_length, counts_length = np.unique(np.array(length), return_counts = True)
 
-length = []
-for i, j in batch:
-    length.append(len(i))
-length = example["aaSeqCDR3"].str.len()
-unique_length, counts_length = np.unique(np.array(length), return_counts = True)
-plt.bar(unique_length, counts_length)
-p_values = counts_length/np.sum(counts_length)
-#normalized on one:
-plt.bar(unique_length, counts_length/np.max(counts_length))
-#p-values
-plt.bar(unique_length, p_values)
+            # add every single subplot to the figure with a for loop
+        ax = fig.add_subplot(Rows, Cols, Position[n])
+        ax.bar(unique_length, counts_length)  # Or whatever you want in the subplot
+        ax.title.set_text(experiment)
+        ax.title.set_size(10)
+        ax.set_ylabel("Read Count", fontsize=6.0)  # Y label
+        ax.set_xlabel('Read Length', fontsize=6.0)  # X label
+        n += 1
+    fig.suptitle('Distribution of number of sequences with certain length')
+    plt.show()
+    plt.tight_layout()
+
+ #   plt.show()
+   # plt.bar(unique_length, counts_length)
+   # p_values = counts_length/np.sum(counts_length)
+    #normalized on one:
+   # plt.bar(unique_length, counts_length/np.max(counts_length))
+    #p-values
+  #  plt.bar(unique_length, p_values)
