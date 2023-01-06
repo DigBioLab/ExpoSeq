6abcfc9e8ea2659c0b90de925e0bd157547a7f08
nilshof01 <s220672@dtu.dk>
2023-01-06 13:52:42 +0100

    clean up clustering code

--- python_scripts/tidy_data/clustering.py
+++ python_scripts/tidy_data/clustering.py
@@ -1,8 +1,7 @@
-#import imnet
+
 from scipy.sparse import csr_matrix
 from networkx.convert_matrix import from_scipy_sparse_matrix
-from numpy import array, empty
-import matplotlib.pyplot as plt
+import numpy as np
 import editdistance
 ## this script generates too many figures and should be run one library at a time
 ## If i am tu use these graphs
@@ -10,37 +9,28 @@ import editdistance
 
 # imnet process_strings.distance matrix from https://github.com/rokroskar/imnet/blob/master/imnet/process_strings.py
 
-#def cleaning(sample_name, report):
- #   sample = report[report["Experiment"] == sample_name]
-  #  aa = list(sample["aaSeqCDR3"])
-   # mat_arr = array(list(imnet.process_strings.distance_matrix(aa, min_ld=0, max_ld=2)))
-    #mat = csr_matrix((mat_arr[:, 2], (mat_arr[:, 0], mat_arr[:, 1])), (len(aa), len(aa)))
-    #G = from_scipy_sparse_matrix(mat)
-    # nx.draw_networkx(G_raph, node_size=10, with_labels=False)
-    #degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
-   # dmax = max(degree_sequence)
-    #return G, degree_sequence
 
-def cleaning(sample_name, report):
-    sample = report[report["Experiment"] == sample_name]
-    aa = list(sample["aaSeqCDR3"])
+def cleaning(report):
+    aa = list(report["aaSeqCDR3"])
     num_strings = len(aa)
-    # Create an empty distance matrix
-    distance_matrix = empty((num_strings, num_strings))
-    # Fill the distance matrix with the Levenshtein distances between the strings,
-    # setting a maximum distance of 2 and a minimum distance of 0
-    max_distance = 2
-    min_distance = 0
-    for i in range(num_strings):
-        for j in range(num_strings):
-            distance = editdistance.distance(aa[i], aa[j])
-            distance_matrix[i, j] = min(max(distance, min_distance), max_distance)
+    string_index = [*range(0, num_strings, 1)]
+    max_ld = 2
+    min_ld = 0
+    collect_coordinates = np.array([0, 0, 0]).reshape(1,3)
+    for i in range(len(string_index)):
+        string1 = aa[i]
+        index_1 = i
+        for index in range(index_1):
+            string2 = aa[index]
+            distance = editdistance.distance(string1, string2)
+            if distance>= min_ld and distance <= max_ld:
+                collect_coordinates = np.concatenate((collect_coordinates,
+                                                      np.array([index_1, index, distance]).reshape(1, 3)))
 
-    mat = csr_matrix((distance_matrix[:, 2], (distance_matrix[:, 0], distance_matrix[:, 1])), (len(aa), len(aa)))
+    mat = csr_matrix((collect_coordinates[:, 2],
+                      (collect_coordinates[:, 0], collect_coordinates[:, 1])),
+                     (len(aa), len(aa)))
     G = from_scipy_sparse_matrix(mat)
-    # plt.figure(figsize=(12,12))
-    # nx.draw_networkx(G_raph, node_size=10, with_labels=False)
     degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
-   # dmax = max(degree_sequence)
     return G, degree_sequence
 
