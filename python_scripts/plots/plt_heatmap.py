fafcfcf7adf8a9b37d2f77046639ad67b60c81a4
nilshof01 <s220672@dtu.dk>
2023-02-04 12:37:40 +0100

    list, array error heatmaps

--- python_scripts/plots/plt_heatmap.py
+++ python_scripts/plots/plt_heatmap.py
@@ -6,17 +6,17 @@ import matplotlib.pyplot as plt
 from python_scripts.tidy_data import heatmaps
 from python_scripts.tidy_data.heatmaps import tidy_jaccard, tidy_sorensen, tidy_morosita_horn, tidy_heatmap_share
 
-def plot_heatmap(sequencing_report, protein, heatmap, ax, specific_experiments = False, rename_from_dic = True):
+def plot_heatmap(sequencing_report, protein, heatmap, ax, specific_experiments = False):
 
     if heatmap == "morosita_horn":
         unique_sequences, unique_experiments = tidy_morosita_horn.cleaning_data(sequencing_report,
-                                                             protein = True,
+                                                             protein = protein,
                                                              specific_experiments = specific_experiments)
         matrix, unique_sequences, unique_experiments = tidy_morosita_horn.morosita_horn_matrix(unique_sequences,
                                                                                                   unique_experiments)
     if heatmap == "jaccard":
         matrix, unique_sequences, unique_experiments = tidy_jaccard.cleaning_jaccard(sequencing_report,
-                                                                                              protein=protein,
+                                                                                    protein=protein,
                                                                                      specific_experiments = specific_experiments)
 
     if heatmap == "sorensen":
@@ -28,14 +28,14 @@ def plot_heatmap(sequencing_report, protein, heatmap, ax, specific_experiments =
                                                                                          protein=protein,
                                                                                         specific_experiments = specific_experiments)
 
-
+    unique_experiments = list(unique_experiments)
     matrix = matrix.sort_index(axis=1)
     matrix = matrix.sort_index(axis = 0)
     matplotlib.use('Qt5Agg')
     sns.heatmap(matrix,
                 ax = ax)
-    plt.xticks(ticks = range(0, unique_experiments.shape[0], 1), labels = unique_experiments, rotation = 45, ha = 'right', size = 5) # create a function which finds the perfect size based on counts of xlabels
-    plt.yticks(ticks = range(0, unique_experiments.shape[0], 1),labels = unique_experiments, va='top', size=5)
+    plt.xticks(ticks = range(0, len(unique_experiments), 1), labels = unique_experiments, rotation = 45, ha = 'right', size = 5) # create a function which finds the perfect size based on counts of xlabels
+    plt.yticks(ticks = range(0, len(unique_experiments), 1),labels = unique_experiments, va='top', size=5)
     savefig = ""
    # while savefig != "Y" or "n":
     #    savefig = input("Do you want to save the figure? Y/n" )
