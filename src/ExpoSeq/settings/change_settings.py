d7ad67b8ebe1db216ee645986451fb2fddfcf847
Nils Hofmann <99137425+nilshof01@users.noreply.github.com>
2023-08-29 20:51:07 +0200

    settings class change

--- src/ExpoSeq/settings/change_settings.py
+++ src/ExpoSeq/settings/change_settings.py
@@ -10,9 +10,54 @@ class Settings:
     def __init__(self):
         self.module_dir = os.path.abspath("")
         self.common_vars =  os.path.join(self.module_dir,"settings", "global_vars.txt")
+        self.font_settings_path = os.path.join(self.module_dir, "settings", "font_settings.txt")
+        self.legend_settings_path = os.path.join(self.module_dir,
+                                        "settings",
+                                        "legend_settings.txt")
+        self.colorbar_settings_path = os.path.join(self.module_dir,
+                                                    "settings",	
+                                                    "colorbar.txt")
+        
+    def check_dirs(self):
+        if not os.path.isdir(os.path.join(self.module_dir, "my_experiments")):
+            print("create my_experiments directory")
+            os.mkdir(os.path.join(self.module_dir, "my_experiments"))
+        if not os.path.isdir(os.path.join(self.module_dir, "temp")):
+            print("create temp directory")
+            os.mkdir(os.path.join(self.module_dir, "temp"))
+        if not os.path.isdir(os.path.join(self.module_dir, "settings")):
+            print("create settings directory")
+            os.mkdir(os.path.join(self.module_dir, "settings"))
+            
+    def create_global_vars(self):
+        global_params = {'mixcr_path': '', 'last_experiment': '', 'api_gpt3': '', 'region_of_interest': '', 'RAM': '', 'clustalw_path': ''}
+        with open(self.common_vars, "w") as f:
+            f.write(str(global_params))
+    
+    def create_font_settings(self):
+        
+        font_settings = {'fontfamily': 'serif', 'fontsize': '18', 'fontstyle': 'normal', 'fontweight': 'bold'}
+        with open(self.font_settings_path, "w") as f:
+            f.write(str(font_settings))
+            
+    def create_legend_settings(self):
+        legend_params = {'loc': 'upper right', 'bbox_to_anchor': (1, 1), 'ncols': 1, 'fontsize': 16, 'frameon': True, 'framealpha': 1, 'facecolor': 'white', 'mode': None, 'title_fontsize': 'small'}
+        with open(self.legend_settings_path, "w") as f:
+            f.write(str(legend_params))
+            
+    def create_colorbar_settings(self):
+        colorbar_settings = {'cmap': 'inferno', 'orientation': 'vertical', 'spacing': 'proportional', 'extend': 'neither'}
+        with open(self.colorbar_settings_path, "w") as f:
+            f.write(str(colorbar_settings))
+            
+            
+    def read_global_vars(self):
+        if not os.path.isfile(self.common_vars):
+            self.create_global_vars()
         with open(self.common_vars, "r") as f:
             global_params = f.read()
-        self.global_params = literal_eval(global_params)
+        global_params = literal_eval(global_params)
+        return global_params
         
     def change_ram(self,ram):
         self.global_params["RAM"] = ram
@@ -25,18 +70,13 @@ class Settings:
             f.write(str(self.global_params))
             
     def change_mixcr_path(self, path):
+        global_params = self.read_global_vars()
         self.global_params["mixcr_path"] = path
         with open(self.common_vars, "w") as f:
             f.write(str(self.global_params))
             
-    def check_dirs(self):
-        if not os.path.isdir(os.path.join(self.module_dir, "my_experiments")):
-            print("create my_experiments directory")
-            os.mkdir(os.path.join(self.module_dir, "my_experiments"))
-        if not os.path.isdir(os.path.join(self.module_dir, "temp")):
-            print("create temp directory")
-            os.mkdir(os.path.join(self.module_dir, "temp"))
-        
+
+            
     def save_settings(self, path, params):
             with open(path, "w") as f:
                 f.write(str(params))
@@ -46,6 +86,8 @@ class Settings:
             shutil.rmtree(os.path.join(directory_path, item))
             
     def read_font_settings(self):
+        if not os.path.isfile(self.font_settings_path):
+            self.create_font_settings()
         font_settings_path = os.path.join(self.module_dir, "settings", "font_settings.txt")
         assert os.path.isfile(font_settings_path), "The font settings file does not exist in the given filepath"
         with open(font_settings_path, "r") as f:
@@ -54,6 +96,8 @@ class Settings:
         return font_settings
     
     def read_legend_settings(self):
+        if not os.path.isfile(self.legend_settings_path):
+            self.create_legend_settings()
         legend_settings_path = os.path.join(self.module_dir,
                                         "settings",
                                         "legend_settings.txt")
@@ -64,6 +108,8 @@ class Settings:
         return legend_settings
     
     def read_colorbar_settings(self):
+        if not os.path.isfile(self.colorbar_settings_path):
+            self.create_colorbar_settings()
         colorbar_path = os.path.join(self.module_dir,
                                     "settings",
                                     "colorbar.txt")
@@ -82,7 +128,7 @@ class Settings:
 
 
     def reset_pipeline(self):
-        global_params = {'mixcr_path': '', 'last_experiment': '', 'api_gpt3': '', 'region_of_interest': '', 'RAM': 42}
+        global_params = {'mixcr_path': '', 'last_experiment': '', 'api_gpt3': '', 'region_of_interest': '', 'RAM': '', 'clustalw_path': ''}
         self.save_settings(self.global_params, global_params)
         self.save_settings(os.path.join(self.module_dir,"settings","save_settings.txt"), {'fname': '', 'format': 'png', 'dpi': 300})
         legend_params = {"loc": "upper right", #'upper left', 'upper right', 'lower left', 'lower right'
