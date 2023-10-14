b54f546d0eb956744e3164ee6b2a7c8488ae5d34
Nils Hofmann <99137425+nilshof01@users.noreply.github.com>
2023-10-14 20:51:14 +0200

    refactor

--- build/lib/ExpoSeq/tidy_data/tidy_USQ_plot_.py
+++
@@ -1,7 +0,0 @@
-
-def cleaning_data(sequencing_report, samples):
-    sub_table = sequencing_report.loc[sequencing_report["Experiment"].isin(samples)]
-    apply_fun = lambda x: list(x)
-    sub_table = sub_table.groupby("Experiment")[['nSeqCDR3', 'cloneFraction', 'readCount']].agg(apply_fun).reset_index()
-    return sub_table
-
