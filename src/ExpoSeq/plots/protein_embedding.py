cec66d9c004526ffe97c969cf5ac58e852031d96
Nils Hofmann <99137425+nilshof01@users.noreply.github.com>
2024-02-19 12:54:49 +0100

    update protein embedding

--- src/ExpoSeq/plots/protein_embedding.py
+++ src/ExpoSeq/plots/protein_embedding.py
@@ -6,6 +6,10 @@ import warnings
 
 
 class PrepareData:
+    def __init__(self):
+        
+        self.tsne_results = None
+        self.clones = None
     
     @staticmethod
     def logical_check(batch_size, perplexity, pca_components, model, iterations_tsne):
@@ -16,7 +20,7 @@ class PrepareData:
         # more models will follow
         models_all = ["Rostlab/ProstT5_fp16", "Rostlab/prot_t5_xl_uniref50", "Rostlab/prot_t5_base_mt_uniref50", "Rostlab/prot_bert_bfd_membrane", "Rostlab/prot_t5_xxl_uniref50", "Rostlab/ProstT5", "Rostlab/prot_t5_xl_half_uniref50-enc", "Rostlab/prot_bert_bfd_ss3", "Rostlab/prot_bert_bfd_localization", "Rostlab/prot_electra_generator_bfd", "Rostlab/prot_t5_xl_bfd", "Rostlab/prot_bert", "Rostlab/prot_xlnet", "Rostlab/prot_bert_bfd", "Rostlab/prot_t5_xxl_bfd"]
         assert model in models_all, f"Please enter a valid model name which are\n{models_all}. You can find the models at: https://huggingface.co/Rostlab"
-        assert iterations_tsne != 0, "the number of iterations for the tsne algorithm must not be 0."
+        assert iterations_tsne > 250, "The number of iterations must be larger than 250 according to sklearn"
         
     @staticmethod
     def datatype_check(samples, pca_components, perplexity, iterations_tsne, batch_size, model):
@@ -42,16 +46,39 @@ class PrepareData:
                 perplexity = 1
         return pca_components, perplexity
     
+    
+    def return_binding_results(self,  selected_rows, antigens, region_of_interest):
+        if antigens is not None:
+            kds = selected_rows[antigens].max(axis = 1) # if there are multiple values for the same sequence this will find the highest one 
+            self.tsne_results["binding"] = list(kds)
+            ids = selected_rows[antigens].idxmax(axis = 1)
+        else:
+            kds = None
+            ids = None
+        aminoacids = selected_rows[region_of_interest].to_list()
+        experiments_batch = selected_rows["Experiment"]
+        unique_experiments_num = pd.factorize(experiments_batch)[0]
+        self.tsne_results["experiments_factorized"] = list(unique_experiments_num)
+        self.tsne_results["experiments_string"] = list(experiments_batch)
+        self.tsne_results["sequences"] = list(aminoacids)
+        self.tsne_results['sequence_id'] = pd.Series(range(self.tsne_results.shape[0]))
+        return kds, ids
+    
     @staticmethod
     def filter_binding_data(binding_data, region_of_interest, antigens):
         if binding_data is not None:
             merged_columns = [region_of_interest] + antigens
             binding_data = binding_data[merged_columns]
+        return binding_data
     
     def tidy(self, sequencing_report, list_experiments, region_of_interest, antigens = None, batch_size = 300, pca_components = 70,
              perplexity = 25, iterations_tsne = 1000, model_choice = "Rostlab/prot_bert",binding_data = None,cf_column_name = "cloneFraction", sample_column_name = "Experiment"):
         for sample in list_experiments:
             assert sample in list(sequencing_report[sample_column_name].unique()), f"{sample} does not exist"
+        if binding_data is not None:
+            assert region_of_interest in binding_data.columns.to_list(), f"You must have sequences for {region_of_interest} in your binding data"    
+        
+        
         self.logical_check(batch_size,
                            perplexity,
                            pca_components,
@@ -63,9 +90,9 @@ class PrepareData:
                             iterations_tsne, 
                             batch_size,
                             model_choice)
-        self.filter_binding_data(binding_data,
-                                 region_of_interest, 
-                                 antigens)
+        binding_data = self.filter_binding_data(binding_data,
+                                                region_of_interest, 
+                                                antigens)
         Transformer = TransformerBased(choice = model_choice)
         sequences,sequences_filtered, selected_rows = Transformer.filter_sequences(sequencing_report,
                                                                                batch_size,
@@ -78,14 +105,12 @@ class PrepareData:
         X = Transformer.do_pca(sequences, batch_size, pca_components)
         peptides = selected_rows[region_of_interest].to_list()
         self.clones = selected_rows[cf_column_name]
-        tsne_results = Transformer.do_tsne(X, perplexity, iterations_tsne)
-        return peptides, selected_rows, tsne_results
+        self.tsne_results = Transformer.do_tsne(X, perplexity, iterations_tsne)
+        kds, ids = self.return_binding_results(selected_rows, antigens, region_of_interest)
+        return peptides, selected_rows, kds, ids
     
-    @staticmethod
-    def make_csv(selected_rows, tsne_results):
-        selected_rows["tsne1"] = tsne_results["tsne1"]
-        selected_rows["tsne2"] = tsne_results["tsne2"]
-        selected_rows.to_csv("tsne_results.csv")
+    def make_csv(self):
+        self.tsne_results.to_csv("tsne_results.csv")
         
 
 
@@ -97,7 +122,7 @@ class PlotEmbedding:
         self.ax = ax
         self.binding_data = binding_data
         self.data_prep  = PrepareData()
-        peptides, selected_rows, tsne_results = self.data_prep.tidy(sequencing_report=sequencing_report,
+        peptides, selected_rows, kds, ids = self.data_prep.tidy(sequencing_report=sequencing_report,
                                                             list_experiments=list_experiments,
                                                             region_of_interest=region_of_interest,
                                                             antigens = antigens,
@@ -109,13 +134,13 @@ class PlotEmbedding:
                                                             binding_data=binding_data)
         if self.ax != None:
             if self.binding_data is not None:
-                self.create_binding_plot(selected_rows, antigens, region_of_interest, toxin_names, colorbar_settings)
+                self.create_binding_plot(kds, ids, toxin_names, colorbar_settings)
                 if extra_figure == True and font_settings != {}:
                     self.create_second_bind_plot(font_settings)
                     title = "\n".join(wrap(f"t-SNE embedding for {antigens}", 40))
                     self.ax.set_title(title, pad= 12, **font_settings)
             else:
-                self.create_plot(tsne_results, selected_rows)
+                self.create_plot(selected_rows)
                 if legend_settings != {}:
                     self.add_legend(list_experiments, legend_settings)
                 title = "\n".join(wrap("t-SNE embedding for given samples", 40))
@@ -126,13 +151,14 @@ class PlotEmbedding:
             self.ax.set_ylabel("t-SNE2", **font_settings)
 
             if strands == True:
-                self.add_seq_anotation(peptides, tsne_results)
+                self.add_seq_anotation(peptides, self.data_prep.tsne_results)
             if add_clone_size != None:
                 self.add_size(add_clone_size)
             
     
         
-    def create_plot(self, tsne_results, selected_rows):
+    def create_plot(self, selected_rows):
+        tsne_results = self.data_prep.tsne_results
         experiments_batch = selected_rows["Experiment"]
         self.tsne_plot = self.ax.scatter(tsne_results.tsne1,
                                     tsne_results.tsne2,
@@ -163,28 +189,16 @@ class PlotEmbedding:
                         )
             
 
-    def return_binding_results(self,  selected_rows, antigens, region_of_interest):
-        kds = selected_rows[antigens].max(axis = 1) # if there are multiple values for the same sequence this will find the highest one 
-        ids = selected_rows[antigens].idxmax(axis = 1)
-        aminoacids = selected_rows[region_of_interest].to_list()
-        experiments_batch = selected_rows["Experiment"]
-        unique_experiments_num = pd.factorize(experiments_batch)[0]
-        self.tsne_results["experiments_factorized"] = list(unique_experiments_num)
-        self.tsne_results["experiments_string"] = list(experiments_batch)
-        self.tsne_results["binding"] = list(kds)
-        self.tsne_results["sequences"] = list(aminoacids)
-        self.tsne_results['sequence_id'] = pd.Series(range(self.tsne_results.shape[0]))
-        return kds, ids
     
     def create_second_bind_plot(self, font_settings):
-
+        tsne_results = self.data_prep.tsne_results
         fig2 = plt.figure()
         ax2 = fig2.gca()
-        ax2.scatter(self.tsne_results.tsne1, self.tsne_results.tsne2, alpha = 0.0)
+        ax2.scatter(tsne_results.tsne1, tsne_results.tsne2, alpha = 0.0)
         ax2.set_xlabel('t-SNE 1', **font_settings)
         ax2.set_ylabel('t-SNE 2', **font_settings)
         n = 0
-        for j, row in self.tsne_results.iterrows():
+        for j, row in tsne_results.iterrows():
             if row["binding"] > 1:
                     ax2.text(row['tsne1'], row['tsne2'], row['sequence_id'], fontsize=10, weight = "bold")
 
@@ -194,16 +208,16 @@ class PlotEmbedding:
                     ax2.text(row['tsne1'], row['tsne2'], row['sequence_id'], fontsize=8)
             n += 1
     
-    def create_binding_plot(self, selected_rows, antigens, region_of_interest, toxin_names, colorbar_settings):
-        kds, ids = self.return_binding_results(selected_rows, antigens, region_of_interest)
-        self.tsne_plot = self.ax.scatter(self.tsne_results.tsne1,
-                            self.tsne_results.tsne2,
-                            c = self.tsne_results.binding,
+    def create_binding_plot(self, kds, ids, toxin_names, colorbar_settings):
+        tsne_results = self.data_prep.tsne_results
+        self.tsne_plot = self.ax.scatter(tsne_results.tsne1,
+                            tsne_results.tsne2,
+                            c = tsne_results.binding,
                             alpha = 1,
                             cmap = "magma"
                             )
-        x_cor = list(self.tsne_results.tsne1.iloc[:, 0])
-        y_cor = list(self.tsne_results.tsne2.iloc[:, 0])
+        x_cor = list(tsne_results.tsne1.iloc[:, 0])
+        y_cor = list(tsne_results.tsne2.iloc[:, 0])
         if toxin_names == True:
             for i, txt in enumerate(list(ids)):
                 if list(kds)[i] > 0:
--- src/ExpoSeq/software_tests/test_new/test_protein_embedding.py
+++ src/ExpoSeq/software_tests/test_new/test_protein_embedding.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.ExpoSeq.plots.protein_embedding import PrepareData, PlotEmbedding
 import matplotlib.pyplot as plt
+import numpy as np 
 
 def test_ProteinEmbedding():
     
@@ -9,31 +10,71 @@ def test_ProteinEmbedding():
     sequencing_report["cloneFraction"] = sequencing_report["readFraction"] 
     fig = plt.figure(1, figsize = (12, 10))
     ax = fig.gca()
-    PlotEmbedding(sequencing_report=sequencing_report,
-                model_choice="Rostlab/prot_bert", 
-                list_experiments=["GeneMind_1"],
-                region_of_interest="aaSeqCDR3",
-                strands = False, 
-                add_clone_size=None,
-                batch_size = 200, 
-                pca_components=70,
-                perplexity=25, 
-                iterations_tsne=1000,
-                antigens = None, 
-                ax = ax)
+    list_experiments = ["GeneMind_1"]
+    PrepData = PrepareData()
+    peptides, selected_rows, kds, ids = PrepData.tidy(sequencing_report, list_experiments, "aaSeqCDR3", batch_size = 80, iterations_tsne = 251)
+    assert isinstance(PrepData.tsne_results, pd.DataFrame)
+    result_cols = PrepData.tsne_results.columns
+    assert "binding" not in result_cols
+    assert "sequences" in result_cols
+    assert "sequence_id" in result_cols
+    assert "tsne1" in result_cols
+    assert "tsne2" in result_cols
+    tsne_results = PrepData.tsne_results
+    assert tsne_results.shape[0] == 80
+    assert kds == None
+    assert ids == None
+    grouped = sequencing_report.groupby('Experiment')
+    # Initialize an empty list to store the top sequences for each sample
+    top_sequences = []
+    top_n = 10
+    binding_data = pd.DataFrame(columns=[ 'aaSeqCDR3', 'Antigen 1'])
+    # Iterate over each group (sample) and select the top 10 sequences
+    for sample, group_data in grouped:
+        top_sequences_per_sample = group_data['aaSeqCDR3'].value_counts().nlargest(top_n).index.tolist()
+        values = np.random.randint(low=1000, high=1000000, size=len(top_sequences_per_sample))  
+        sample_values = pd.DataFrame({'aaSeqCDR3': top_sequences_per_sample, 'Antigen 1': values})
+        top_sequences.extend(top_sequences_per_sample)
+        binding_data = pd.concat([binding_data, sample_values])
+        
+    PrepBinding = PrepareData()
+    peptides, selected_rows, kds, ids = PrepBinding.tidy(sequencing_report, list_experiments, region_of_interest="aaSeqCDR3", batch_size = 80, iterations_tsne = 251, binding_data = binding_data, 
+                                                    antigens=["Antigen 1"])
+    tsne_binding = PrepBinding.tsne_results
+    result_cols = tsne_binding.columns
+    assert "binding" in tsne_binding
+    assert kds is not None
+    assert ids is not None
+    
+
+    
+    
+    
+#    PlotEmbedding(sequencing_report=sequencing_report,
+#                model_choice="Rostlab/prot_bert", 
+#                list_experiments=["GeneMind_1"],
+#                region_of_interest="aaSeqCDR3",
+ #               strands = False, 
+ #               add_clone_size=None,
+ #               batch_size = 200, 
+ #               pca_components=70,
+ #               perplexity=25, 
+ #               iterations_tsne=1000,
+ #               antigens = None, 
+ #               ax = ax)
     
     
 
-    PlotEmbedding(sequencing_report=sequencing_report,
-                model_choice="Rostlab/prot_bert", 
-                list_experiments=["GeneMind_1"],
-                region_of_interest="aaSeqCDR3",
-                strands = False, 
-                add_clone_size=None,
-                batch_size = 200, 
-                pca_components=70,
-                perplexity=25, 
-                iterations_tsne=1000,
-                antigens = None,
-                ax = ax,
-                )
+ #   PlotEmbedding(sequencing_report=sequencing_report,
+ #               model_choice="Rostlab/prot_bert", 
+ #               list_experiments=["GeneMind_1"],
+ #               region_of_interest="aaSeqCDR3",
+ #               strands = False, 
+ #               add_clone_size=None,
+ #               batch_size = 200, 
+ #               pca_components=70,
+ #               perplexity=25, 
+ #               iterations_tsne=1000,
+ #               antigens = None,
+ #               ax = ax,
+ #               )
